149bdf3de9ad553a0e166f104ff6f564f616b8e1
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-09-06 18:02:19 +0200

    added new jsonp request

--- src/app/services/musicArtist/music-artist-details-http.service.ts
+++ src/app/services/musicArtist/music-artist-details-http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,15 @@ export class MusicArtistDetailsHttpService {
   getArtistDetailsById(id?: string | null): Observable<any> {
     // return this.http.get('/api/artist/' + id);//https://api.deezer.com
     // return this.http.get('https://api.deezer.com/artist/' + id);//https://api.deezer.com
-    return this.http.jsonp('https://api.deezer.com/artist/' + id + '?output=jsonp&callback=ng_jsonp_callback_0', "JSONP_CALLBACK");
+
+    // return this.http.jsonp('https://api.deezer.com/artist/' + id + '?output=jsonp&callback=ng_jsonp_callback_0', "JSONP_CALLBACK");
+
+    return this.http.jsonp('https://api.deezer.com/artist/' + id + '?output=jsonp&callback=ng_jsonp_callback_0', 'a');//.pipe(map(this.a)) //(this.extractData);;//, "JSONP_CALLBACK");
     // return this.http.get('https://catfact.ninja/fact')
+  }
+
+  a(b: any) {
+    console.log(b)
   }
 
   getArtistAlbumsByArtistName(name: string | null): Observable<any> { // MusicAlbum
